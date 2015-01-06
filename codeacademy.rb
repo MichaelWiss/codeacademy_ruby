cf3570030fc3c0495ea784fb7b8677561e0d691a
Michael Wiss <michael.wiss@gmail.com>
2015-01-05 18:42:26 -0600

    object oriented ruby exercise

--- codeacademy.rb
+++ codeacademy.rb
@@ -1,50 +1,77 @@
-movies = {TaxiDriver:"4"}
-puts "What is your favorite movie?"
-choice = gets.chomp
-puts "What would you like to do?"
-puts "-- Type 'add' to add a movie."
-puts "-- Type 'update' to update a movie."
-puts "-- Type 'display' to display all movies."
-puts "-- Type 'delete' to delete a movie."
+# movies = {TaxiDriver:"4"}
+# puts "What is your favorite movie?"
+# choice = gets.chomp
+# puts "What would you like to do?"
+# puts "-- Type 'add' to add a movie."
+# puts "-- Type 'update' to update a movie."
+# puts "-- Type 'display' to display all movies."
+# puts "-- Type 'delete' to delete a movie."
 
-choice = gets.chomp
-case choice
-when "add"
-  puts "What would you want to add?"
-  title = gets.chomp
-  if  movies[title.to_sym].nil?
-  puts "What would you rate the movie?"
-  rating = gets.chomp
-      movies[title.to_sym] = rating.to_i
-  puts "The #{title} was added with a rank of #{rating}!"
-  else
-  puts "This movie already exists!"
-  end
-when "update"
-  puts "What movie would you like to update?"
-  title = gets.chomp
-  if movies[title.to_sym].nil?
-      puts "The movie is not in the database."
-    else
-    puts "What is your new rating of the movie?"
-    rating = gets.chomp
-    movies[title.to_sym] = rating.to_i
-    puts "This is the new rating: #{rating}"
-end
+# choice = gets.chomp
+# case choice
+# when "add"
+#   puts "What would you want to add?"
+#   title = gets.chomp
+#   if  movies[title.to_sym].nil?
+#   puts "What would you rate the movie?"
+#   rating = gets.chomp
+#       movies[title.to_sym] = rating.to_i
+#   puts "The #{title} was added with a rank of #{rating}!"
+#   else
+#   puts "This movie already exists!"
+#   end
+# when "update"
+#   puts "What movie would you like to update?"
+#   title = gets.chomp
+#   if movies[title.to_sym].nil?
+#       puts "The movie is not in the database."
+#     else
+#     puts "What is your new rating of the movie?"
+#     rating = gets.chomp
+#     movies[title.to_sym] = rating.to_i
+#     puts "This is the new rating: #{rating}"
+# end
         
-when "display"
-  movies.each do |m, r| 
-      puts "#{m}: #{r}"
+# when "display"
+#   movies.each do |m, r| 
+#       puts "#{m}: #{r}"
+#   end
+# when "delete"
+#   puts "Which movie would you like to delete?"
+#   title = gets.chomp
+#   if  movies[title.to_sym].nil?
+#       puts "The movie is not in the database."
+#   else
+#       movies.delete(title)
+#       puts "The #{title} has been deleted."
+#   end
+# else 
+#   puts "Error!"
+# end
+
+
+
+#Object Oriented Ruby
+
+
+class Message
+    @@messages_sent = 0
+    def initialize(from, to)
+        @from = from
+        @to = to
+        @@messages_sent +=1
   end
-when "delete"
-  puts "Which movie would you like to delete?"
-  title = gets.chomp
-  if  movies[title.to_sym].nil?
-      puts "The movie is not in the database."
-  else
-      movies.delete(title)
-      puts "The #{title} has been deleted."
+end  
+
+my_message = Message.new(3, 17)
+class Email < Message
+  def initialize(from, to)
+     super
   end
-else 
-  puts "Error!"
-end
+ end
+
+
+
+
+
+
