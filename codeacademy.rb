17eee99dcf4621489a7dc1d2544236bfe02d1b69
Michael Wiss <michael.wiss@gmail.com>
2015-01-23 09:52:18 -0600

    object oriented js

--- codeacademy.rb
+++ codeacademy.rb
@@ -513,6 +513,44 @@ for (var i = 0; i < languages.length; i++){
 
 
 
+var friends = {
+    bill: {
+        firstName: "Bill",
+        lastName: "Gates",
+        number: "(206) 555-5555",
+        address: ['11 Microsoft Way', 'Redmond', 'WA', '98052']
+    },
+    steve: {
+        firstName: "Steve",
+        lastName: "Wonder",
+        number: "(207) 544-4444",
+        address: ['23', 'Soul Ave', 'Detroit', 'MI', '34333']
+    }
+}
+
+var list = function(friends){
+    for (var key in friends) {
+    console.log(key);
+}
+}
+
+
+list(friends);
+
+
+
+var search = function(name) {
+    for (var key in friends) {
+        if(friends[key].firstName === name){
+            console.log(friends[key]);
+            return friends[key];
+        }
+    }
+};
+search("Steve");
+
+
+
 
 
 
