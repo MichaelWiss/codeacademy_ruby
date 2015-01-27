164896edd8edb44e09d5e7332d4c69b93cd78731
Michael Wiss <michael.wiss@gmail.com>
2015-01-26 18:09:39 -0600

    js objects

--- codeacademy.rb
+++ codeacademy.rb
@@ -551,6 +551,17 @@ search("Steve");
 search("Bill");
 search("Marianna");
 
+#javascript objects
+var snoopy = {
+    species: "beagle",
+    age: 10,
+};
+
+var buddy = new Object();
+buddy.species = "golden retriever";
+buddy.age = 5;
+
+
 
 
 
