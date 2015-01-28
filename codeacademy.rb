9e4c0d3f0b2426775679960a9a7410626ff1261b
Michael Wiss <michael.wiss@gmail.com>
2015-01-28 17:55:16 -0600

    more js

--- codeacademy.rb
+++ codeacademy.rb
@@ -591,6 +591,21 @@ susan.setAge = setAge;
 susan.setAge(35);
 
 
+var rectangle = new Object();
+rectangle.height = 3;
+rectangle.width = 4;
+// here is our method to set the height
+rectangle.setHeight = function (newHeight) {
+  this.height = newHeight;
+};
+// help by finishing this method
+rectangle.setWidth = function (newWidth){
+    this.width = newWidth;
+};
+  
+rectangle.setWidth(8);
+rectangle.setHeight(6);
+
 
 
 
