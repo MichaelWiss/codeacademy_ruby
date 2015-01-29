96f0779762212801ca6b9f0ca2deaaedc9abf700
Michael Wiss <michael.wiss@gmail.com>
2015-01-28 20:17:45 -0600

    js

--- codeacademy.rb
+++ codeacademy.rb
@@ -621,6 +621,35 @@ var sam =new Cat(3, "black");
 var george =new Dog(4, "brown" );
 console.log(sam, george);
 
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
+}
+
+// now we can easily make all of our rabbits
+var rabbit1 = new Rabbit("fluffy");
+var rabbit2 = new Rabbit("happy");
+var rabbit3 = new Rabbit("sleepy");
+rabbit1.describeMyself();
+rabbit2.describeMyself();
+rabbit3.describeMyself();
+
+// Our person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// Now we can make an array of people
+var family = new Array();
+family[0] = new Person("alice", 40);
+family[1] = new Person("bob", 42);
+family[2] = new Person("michelle", 8);
+family[3] = new Person("timmy", 6);
+
+
 
 
 
