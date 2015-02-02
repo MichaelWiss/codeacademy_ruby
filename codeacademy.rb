65f2b66f5645bd3e5720b44c012e2fbf136f451b
Michael Wiss <michael.wiss@gmail.com>
2015-02-02 06:52:38 -0600

    typeof

--- codeacademy.rb
+++ codeacademy.rb
@@ -783,6 +783,17 @@ function Person(job, married) {
 var gabby = new Person("student", true);
 
 
+// complete these definitions so that they will have
+// the appropriate types
+var anObj = { job: "I'm an object!" };
+var aNumber = 42;
+var aString = "I'm a string!";
+
+console.log(typeof anObj  ); // should print "object"
+console.log(typeof aNumber  ); // should print "number"
+console.log(typeof aString  ); // should print "string"
+
+
 
 
 
