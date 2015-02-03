b294d9a94a37fa9a4cadb2057447d89dad4901b6
Michael Wiss <michael.wiss@gmail.com>
2015-02-03 10:22:52 -0600

    for in loop

--- codeacademy.rb
+++ codeacademy.rb
@@ -794,6 +794,18 @@ console.log(typeof aNumber  ); // should print "number"
 console.log(typeof aString  ); // should print "string"
 
 
+var nyc = {
+    fullName: "New York City",
+    mayor: "Bill de Blasio",
+    population: 8000000,
+    boroughs: 5
+};
+
+for(var key in nyc) {
+    console.log(key);
+}
+
+
 
 
 
