bfef2506a8a437f08a8c15ff8a0bed08da965620
Michael Wiss <michael.wiss@gmail.com>
2015-02-04 11:37:48 -0600

    js objects

--- codeacademy.rb
+++ codeacademy.rb
@@ -806,6 +806,24 @@ for(var key in nyc) {
 }
 
 
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+// a function that prints the name of any given person
+var printPersonName = function (p) {
+  console.log(p.name);
+};
+
+var bob = new Person("Bob Smith", 30);
+printPersonName(bob);
+
+// make a person called me with your name and age
+// then use printPersonName to print your name
+var me = new Person("Michael", 42);
+printPersonName(me);
+
+
 
 
 
