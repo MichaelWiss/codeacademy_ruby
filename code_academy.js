f6279341d1906a66b37923546ec4b427459f85b9
Michael Wiss <michael.wiss@gmail.com>
2015-05-26 10:37:24 -0500

    like click function

--- code_academy.js
+++ code_academy.js
@@ -832,5 +832,41 @@ app.controller('MainController', ['$scope', function($scope) {
 }
 }]);
        
+/* angular like click function */
+
+app.controller('MainController', ['$scope', function($scope) { 
+  $scope.title = 'Top Developer Manuals in Books';
+  $scope.promo = '50% off';
+  $scope.products = [ 
+  { 
+    name: 'The Book of Trees', 
+    price: 19, 
+    pubdate: new Date('2014', '03', '08'), 
+    cover: 'img/the-book-of-trees.jpg', 
+    likes: 0,
+  }, 
+  { 
+    name: 'Program or be Programmed', 
+    price: 8, 
+    pubdate: new Date('2013', '08', '01'), 
+    cover: 'img/program-or-be-programmed.jpg',
+    likes: 0,
+  },
+    {
+    name: 'The Forever War',
+    price: 20,
+    likes: 0,
+    },
+    {
+    name: 'Javascript & Jquery',
+    price: 20,
+    likes: 0,
+    }
+]
+  $scope.plusOne = function(index) { 
+  $scope.products[index].likes += 1; 
+};
+ 
+}]);
 
 
